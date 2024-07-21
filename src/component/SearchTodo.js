58daedc0128cb074344f7ed5bf27c1eda6dc9ea1
Yoto Kim <yongtokim1215@gmail.com>
2024-07-21 14:57:38 -0700

    Added user authentication

--- src/component/SearchTodo.js
+++ src/component/SearchTodo.js
@@ -28,7 +28,8 @@ class SearchTodo extends Component {
       },
       params: {
         taskname: this.state.content
-      }
+      },
+      withCredentials: true
     }).then(res => {
       this.setState({
         tmpdata: JSON.stringify(res.data),
